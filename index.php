f80249467cef1a84611c1af5571e3ecfccbc586a
susannews <Susanne@Susannes-MacBook-Pro.local>
2017-05-24 09:31:37 +0200

    Added bootstrap & angular

--- index.php
+++ index.php
@@ -7,8 +7,31 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Assignment III</title>
+	
+	<!-- Angular -->
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+	
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	
+	<!-- Our stylesheet -->
+	<link rel="stylesheet" href="styles/stylesheet.css">
 </head>
 <body>
-	ASS-signment 3 is happening here
+	
+	<div class="container-fluid">
+      <div ng-app="">
+ 
+		<p>Input something in the input box:</p>
+		<p>Name : <input type="text" ng-model="name" placeholder="Enter name here"></p>
+		<h1>Hello {{name}}</h1>
+
+		</div>
+   </div>	
+	
+	<!-- Bootstrap JavaScript plugins, jQuery, and Tether -->
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </body>
 </html>
